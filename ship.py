11c04688439f4620fa991c08f0a1c6c1b6f955b6
Eduardo Júnio <eduardojvr10@gmail.com>
2018-05-02 17:55:38 -0300

    Corrigindo indentação

--- ship.py
+++ ship.py
@@ -10,8 +10,7 @@ class Ship():
         self.screen_rect = screen.get_rect()
         # inicia cada nova espaçonave na parte inferior central da tela
         self.rect.centerx = self.screen_rect.centerx 
-        self.rect.bottom = self.screen_rect.bottom
-    
+        self.rect.bottom = self.screen_rect.bottom    
 
     # Desenha a espaçonave em sua posição atual
     def blitme(self):
